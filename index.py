bed915b1c49ce6fb623e71af16ec65dd0a209c81
ivanpizarro <ivan.pizarro@hotmail.com>
2017-06-07 10:37:47 -0400

    Ingresa a weplay.cl, adquiere datos de nombre y precio de productos

--- index.py
+++ index.py
@@ -25,29 +25,7 @@ precio = soup.find_all('div', class_="prec_prod")
 <<<<<<< HEAD
 print(nombre[1].text)
 print(precio[1].text)
-=======
-if status_code == 200:
-	
-	#pasamos el contenido HTML de la web a un objeto Bs4()
-	html = BeautifulSoup(req.text,"html.parser")
 
-	#Obtecion de todos los divs donde estan las entradas
-	entradas = html.find_all('div',{'class': 'col-md-4 col-cs-12'})
-
-	#recorremos las entradas para extraer el titulo, autor y fecha
-	for i, entrada in enumerate(entradas):
-		#con el metodo "getText()" no nos devuelve el HTML
-		titulo= entrada.find('span',{'class':'tituloPost'}).getText()
-		#sino llamaos al metodo "getText()" nos devuelve tambien el HTML
-		autor = entrada.find('span',{'class':'autor'})
-		fecha = entrada.find('span',{'class':'fecha'}).getText()
-
-		#imprimir titulo, autor y fecha de entradas
-		print "%d - %s | %s | %s" % (i+1,titulo,autor,fecha)
-else:
-	print "Status Code %d" % status_code
-
-# comentario de prueba
 
 
 
