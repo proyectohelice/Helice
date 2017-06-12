496914314be53cb3860c27ce6b527c24269787cc
ignacio delgadillo <ignaciodelgadillo@MBP-de-ignacio.localdomain>
2017-06-12 09:07:02 -0400

    se agregan comentarios de funcionamiento a saludo.py

--- saludo.py
+++ saludo.py
@@ -1,9 +1,9 @@
-def salute(hora):
-    if hora<12 and hora>0:
+def salute(hora):#funcion en la cual toma el valor de la variable hola
+    if hora<12 and hora>0:#condicion en la cual verifica el valor y saluda de acuerdo al horario del dia
         print('Muy buenos dias, por favor seleccione un producto a consultar del siguiente menu:')
     elif hora>=12 and hora<20:
         print('Muy buenas tardes, por favor seleccione un producto a consultar del siguiente menu:')
     elif hora>=20:
         print('Muy buenas noches, por favor seleccione un producto a consultar del siguiente menu:')
-from datetime import datetime, date, time, timedelta
-salute(datetime.now().hour)
+from datetime import datetime, date, time, timedelta#importa datetime que contiene el valor de la hora actual
+salute(datetime.now().hour)#le asigna el valor de datetime a la variable hora para entrar a la funcion e iniciarla
