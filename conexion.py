bcfb48d2239daf0d39d1d3894bc46cf7230ac3aa
ignacio delgadillo <ignaciodelgadillo@MBP-de-ignacio.localdomain>
2017-06-12 09:17:43 -0400

    agrego comentarios de funcionamiento a la funcion conexion.py

--- conexion.py
+++ conexion.py
@@ -1,14 +1,14 @@
-import socket
+import socket# se importa la funcion socket para comprobar conexion
 
-def IsInternetUp():
-    testConn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+def IsInternetUp():#funcion que contiene las try o except para mostrar el resultado de la conexion
+    testConn = socket.socket(socket.AF_INET,socket.SOCK_STREAM)#crea un socket INET del tipo STREAM
     try:
-        testConn.connect(('http://www.apolinav.cl', 80))
+        testConn.connect(('http://www.apolinav.cl', 80))#se conecta al servidor web por el puerto 80
         print ("Estamos on-line. Ya podemos iniciar el programa.")
         testConn.close()
     except:
         print ("Lo sentimos, pero no se ha podido establecer la conexión.")
-        quit()
+        quit()#en el caso que no exista conexion, la funcion interrumpe el programa
     testConn.close()
 
-IsInternetUp()
+IsInternetUp()#ejecuta la funcion
